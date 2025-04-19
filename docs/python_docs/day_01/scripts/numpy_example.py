a620024c35a6aeb592a14ecd29b3d337652a7f01
dduyanhhoang <dduyanhhoang@gmail.com>
2025-04-19 19:04:36 +0700

    docs(lesson/python/day 1): add python scripts for function, remove redundant ones

--- docs/python_docs/day_01/scripts/numpy_example.py
+++ docs/python_docs/day_01/scripts/numpy_example.py
@@ -15,4 +15,6 @@ print("Normalized:\n", normalized_image)
 
 
 print("Mean after normalization:", np.mean(normalized_image))
+# Mean after normalization: -2.503490748040349e-16 ~0
 print("Std after normalization:", np.std(normalized_image))
+# Std after normalization: 0.9999999999999998 ~1
